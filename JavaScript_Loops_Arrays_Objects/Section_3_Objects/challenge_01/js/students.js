4bde1f266fd07f69eec3fe01501b35c723e2b812
Haico27 <haico.wensink@gmail.com>
2017-03-04 17:32:03 +0100

    Create array with studentobjects

---
+++ JavaScript_Loops_Arrays_Objects/Section_3_Objects/challenge_01/js/students.js
@@ -0,0 +1,7 @@
+var students = [
+  {name: 'Melvin' , track: 'Ruby on Rails' , achievements: 5 , points: 530 },
+  {name: 'Richard', track: 'React', achievements: 6 , points: 790 },
+  {name: 'Marcia' , track: 'JavaScript', achievements: 7 , points: 800 },
+  {name: 'Klaas', track: 'FeathersJS', achievements: 8, points: 650 },
+  {name: 'Josefien' , track: 'MongoDB' , achievements: 3, points: 300 },
+];
